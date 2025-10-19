a2ef822bcbb0a6e927f81103234b449cc22218f6
maxrangner <max@rangner.com>
2025-10-19 11:40:49 +0200

    Work on labb fileCpp.cpp

--- 02-Programmering-C-och-Cpp/Labb/file/fileCpp.cpp
+++ 02-Programmering-C-och-Cpp/Labb/file/fileCpp.cpp
@@ -2,44 +2,53 @@
 #include <vector>
 #include <fstream>
 
-bool writeToFile(std::vector<float> inpVec) {
+bool writeToFile(std::vector<float>& myInpVec) {
     std::ofstream outFile("data.txt");
     if (!outFile) return false;
-    for (float f : inpVec){
+    for (float f : myInpVec){
         outFile << f << std::endl;
     }
     outFile.close();
     return true;
 }
 
-bool readFromFile(float temp) {
-    std::ofstream inFile("data.txt");
+bool readFromFile(std::vector<float>& myOutVec) {
+    std::ifstream inFile("data.txt");
     if (!inFile) return false;
-    inFile << temp << std::endl;
+
+    std::string newString;
+    while(std::getline(inFile, newString)) {
+        myOutVec.push_back(std::stof(newString));
+    }
     inFile.close();
     return true;
 }
 
 int main() {
-    std::vector<float> myVec;
+    std::vector<float> myInpVec {};
+    std::vector<float> myOutVec {};
+    int sum = 0;
 
+    // Get user input
     for (int i = 0; i < 5; i++) {
         std::string usrInp;
         std::cout << "> ";
         std::cin >> usrInp;
-        myVec.push_back(stof(usrInp));
+        myInpVec.push_back(stof(usrInp));
     }
 
-    if (writeToFile(myVec)) std::cout << "Success!\n";
+    // Write to the file
+    if (writeToFile(myInpVec)) std::cout << "Success writing file!\n";
     else std::cout << "Error writing to file.\n";
 
-    std::ifstream inFile("data.txt");
+    // Read from the file
+    if (readFromFile(myOutVec)) std::cout << "Success reading file!\n";
+    else std::cout << "Error writing to file.\n";
 
-    std::string readLine;
-    int sum = 0;
-    while(std::getline(inFile, readLine)) {
-        std::cout << readLine << std::endl;
-        sum += std::stof(readLine);
+    // Print vector and calculate sum
+    for (float f : myOutVec) {
+        std::cout << f << "\n";
+        sum += f;
     }
 
     std::cout << "Sum: " << sum;
