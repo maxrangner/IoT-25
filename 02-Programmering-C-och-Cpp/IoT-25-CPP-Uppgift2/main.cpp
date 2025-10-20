733d7e49966a8d48a2cab71a5c7395702ae80cfa
maxrangner <max@rangner.com>
2025-10-20 13:03:28 +0200

    Uppgift 2: Create first prototype.

--- 02-Programmering-C-och-Cpp/IoT-25-CPP-Uppgift2/main.cpp
+++ 02-Programmering-C-och-Cpp/IoT-25-CPP-Uppgift2/main.cpp
@@ -1,120 +1,99 @@
 #include <iostream>
 #include <array>
 #include <vector>
+#include <map>
+#include <ctime>
 #include <fstream>
 
+struct DataPoint {
+    float temperature {};
+    float humidity {};
+    int timestamp {};
+};
 
 constexpr int NUM_DATAPOINTS = 5;
-
-struct DataPoint {
-    int deviceId;
-    std::string type;
-    float temperature;
-};
 
 class MySensor {
     private:
         int deviceId;
         std::string type;
-        float temperature;
+        float measuredVal;
     public:
-        MySensor() : deviceId(0), type("None"), temperature(0.0f) {}
-        MySensor(int i) : deviceId(i) {}
-        MySensor(int i, std::string t, float temp) : deviceId(i), type(t), temperature(temp) {}
-        void setValue(float newVal) {
-            temperature = newVal;
-        }
-        float readValue() {
-            return temperature;
-        }
-        void printInfo() {
-            std::cout << "DeviceID: " << deviceId << "\n"
-                      << "Type: " << type << "\n"
-                      << "Temperature: " << temperature << std::endl;
-        }
-
+        // Constructors
+        MySensor() : deviceId(0), type("Unknown"), measuredVal(0.0f) {}
+        MySensor(int id) : deviceId(id) {}
+        MySensor(int id, std::string t, float temp) : deviceId(id), type(t), measuredVal(temp) {}
+        
+        void setVal(float newVal) {measuredVal = newVal; }
+        int getId() { return deviceId; }
+        std::string getType() { return type; }
+        float getVal() { return measuredVal; }
 };
 
 class SystemManager {
     private:
         int numSensors;
-        std::vector<MySensor> sensorList;
-        std::vector<std::array<DataPoint, NUM_DATAPOINTS>> database;
+        
+        std::vector<DataPoint> database;
     public:
-        SystemManager() : numSensors(0) {}
-        int getNumSensors() {
-            return numSensors;
-        }
-        int nextSensorId() {
-            numSensors++;
-            return numSensors;
-        }
-        void addSensor(MySensor newSensor) {
-            sensorList.push_back(newSensor);
-        }
-        void listSensors() {
-            std::cout << "************************\n";
-            for (MySensor s: sensorList) {
-                s.printInfo();
-                std::cout << "\n";
-            }
-            std::cout << "************************";
-            std::cout << std::endl;
-        }
-        void collect() {
-            for (std::array<DataPoint, NUM_DATAPOINTS> dp : database) {
+        std::vector<MySensor> sensorsList;
 
+        SystemManager() : numSensors(0) {}
+        int getNumSensors() { return numSensors; }
+        int nextSensorId() { return numSensors++; }
+        void addSensor(MySensor newSensor) { sensorsList.push_back(newSensor);}
+        void takeReadings() {
+            DataPoint newDataPoint;
+            for (MySensor s : sensorsList) {
+                if (s.getType() == "temperature-sensor") newDataPoint.temperature = s.getVal();
+                if (s.getType() == "humidity-sensor") newDataPoint.humidity = s.getVal();
+                newDataPoint.timestamp = 2025;
+                // newDataPoint.timestamp = getTime();
             }
+            database.push_back(newDataPoint);
         }
+        std::vector<DataPoint> getReadings() { return database; }
 };
 
-bool writeToFile(std::vector<float> inpVec) {
-    std::ofstream outFile("data.txt");
-    if (!outFile) return false;
-    for (float f : inpVec){
-        outFile << f << std::endl;
+void printData(std::vector<DataPoint> data) {
+    for (DataPoint dp : data) {
+        std::cout << dp.timestamp << " - "
+                  << "Temperature: " << dp.temperature << " "
+                  << "Humidity: " << dp.humidity << " ";
+        std::cout << std::endl;
     }
-    outFile.close();
-    return true;
-}
-
-bool readFromFile(float temp) {
-    std::ofstream inFile("data.txt");
-    if (!inFile) return false;
-    inFile << temp << std::endl;
-    inFile.close();
-    return true;
 }
 
 int main() {
-    // SystemManager manager;
-    // MySensor s1(manager.nextSensorId(), "Temperature Sensor", 25.6);
-    // MySensor s2(manager.nextSensorId(), "Humidity Sensor", 43.1);
+    SystemManager manager;
+    MySensor s1(manager.nextSensorId(), "temperature-sensor", 25.6);
+    MySensor s2(manager.nextSensorId(), "humidity-sensor", 43.1);
 
-    // manager.addSensor(s1);
-    // manager.addSensor(s2);
-    // manager.listSensors();
+    manager.addSensor(s1);
+    manager.addSensor(s2);
 
-    std::vector<float> myVec;
-
-    for (int i = 0; i < 5; i++) {
-        std::string usrInp;
-        std::cout << "> ";
-        std::cin >> usrInp;
-        myVec.push_back(stof(usrInp));
+    for (MySensor s : manager.sensorsList) {
+        s.setVal(10);
     }
+    manager.takeReadings();
+    printData(manager.getReadings());
 
-    if (writeToFile(myVec)) std::cout << "Success!\n";
-    else std::cout << "Error writing to file.\n";
+    std::cout << "Next reading: \n";
 
-    std::ifstream inFile("data.txt");
+    MySensor s2(manager.nextSensorId(), "humidity-sensor", 43.1);
 
-    std::string readLine;
-    int sum = 0;
-    while(std::getline(inFile, readLine)) {
-        std::cout << readLine << std::endl;
-        sum += std::stof(readLine);
+    for (MySensor s : manager.sensorsList) {
+        s.setVal(20);
     }
+    manager.takeReadings();
 
-    std::cout << "Sum: " << sum;
+    printData(manager.getReadings());
 }
+
+/*
+ISSUES:
+- System can't handle multiple of the same sensor.
+
+BUGS:
+- s.setVal() does not update value.
+*/
