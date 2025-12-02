bb6518a41dbf4d426b438febda5b8bf3e03b639b
maxrangner <max@rangner.com>
2025-12-02 19:54:38 +0100

    Add instructions.

--- 04-Digitalteknik-och-Elektronik/labb/keypad/keypad.ino
+++ 04-Digitalteknik-och-Elektronik/labb/keypad/keypad.ino
@@ -1,3 +1,7 @@
+/*
+COMMENT OUT ONE OF THE MODELS (A or B) BEFORE COMPILING!
+*/
+
 constexpr uint8_t COLUMNS = 2;
 constexpr uint8_t ROWS = 2;
 constexpr uint8_t pinColumn1 = 10;
@@ -44,39 +48,39 @@ void loop() {
   delay(10);
 }
 
-// // MODELL B
-// void setup() {
-//   Serial.begin(9600);
-//   delay(100);
-//   for (auto& c : columnPins) {
-//     pinMode(c, INPUT_PULLUP);
-//   }
-//   for (auto& r : rowPins) {
-//     pinMode(r, OUTPUT);
-//     digitalWrite(r, LOW);
-//   }
-//   Serial.println("BOOT!");
-// }
+// MODELL B
+void setup() {
+  Serial.begin(9600);
+  delay(100);
+  for (auto& c : columnPins) {
+    pinMode(c, INPUT_PULLUP);
+  }
+  for (auto& r : rowPins) {
+    pinMode(r, OUTPUT);
+    digitalWrite(r, LOW);
+  }
+  Serial.println("BOOT!");
+}
 
-// /*
-// COLUMNS == INPUT_PULLUP
-// ROWS = OUTPUT
-// Loop through COLUMNS and look for one with OUTPUT LOW.
-// Then turn one row after another HIGH and look for which one turns LOW column back HIGH.
-// */
-// void loop() {
-//   bool keyFound = false;
-//   for (int c = 0; c < COLUMNS; c++) {
-//     if (digitalRead(columnPins[c]) != LOW) continue;
-//     for (int r = 0; r < ROWS; r++) {
-//       digitalWrite(rowPins[r], HIGH);
-//       if (digitalRead(columnPins[c]) == HIGH) {
-//         Serial.println(keypad[r][c]);
-//         keyFound = true;
-//       }
-//       digitalWrite(rowPins[r], LOW);
-//       if (keyFound) break;
-//     }
-//     if (keyFound) break;
-//   }
-// }
+/*
+COLUMNS == INPUT_PULLUP
+ROWS = OUTPUT
+Loop through COLUMNS and look for one with OUTPUT LOW.
+Then turn one row after another HIGH and look for which one turns LOW column back HIGH.
+*/
+void loop() {
+  bool keyFound = false;
+  for (int c = 0; c < COLUMNS; c++) {
+    if (digitalRead(columnPins[c]) != LOW) continue;
+    for (int r = 0; r < ROWS; r++) {
+      digitalWrite(rowPins[r], HIGH);
+      if (digitalRead(columnPins[c]) == HIGH) {
+        Serial.println(keypad[r][c]);
+        keyFound = true;
+      }
+      digitalWrite(rowPins[r], LOW);
+      if (keyFound) break;
+    }
+    if (keyFound) break;
+  }
+}
