b3db73bb0874fcaf8e30580d3301b55b094a5784
maxrangner <max@rangner.com>
2025-12-06 13:29:55 +0100

    labb/keypad2: Make prototype with working button input.

--- 04-Digitalteknik-och-Elektronik/labb/keypad/keypad.ino
+++ 04-Digitalteknik-och-Elektronik/labb/keypad/keypad.ino
@@ -4,14 +4,17 @@ COMMENT OUT ONE OF THE MODELS (A or B) BEFORE COMPILING!
 
 constexpr uint8_t COLUMNS = 2;
 constexpr uint8_t ROWS = 2;
-constexpr uint8_t pinColumn1 = 10;
-constexpr uint8_t pinColumn2 = 9;
-constexpr uint8_t pinRow1 = 21;
-constexpr uint8_t pinRow2 = 20;
-const int columnPins[COLUMNS] = {pinColumn1, pinColumn2};
-const int rowPins[ROWS] = {pinRow1,pinRow2};
+constexpr uint8_t PIN_COL_1 = 10;
+constexpr uint8_t PIN_COL_2 = 9;
+constexpr uint8_t PIN_ROW_1 = 21;
+constexpr uint8_t PIN_ROW_2 = 20;
+const int columnPins[COLUMNS] = {PIN_COL_1, PIN_COL_2};
+const int rowPins[ROWS] = {PIN_ROW_1,PIN_ROW_2};
 const int keypadValues[ROWS][COLUMNS] = {{1, 2}, {3, 4}};
 bool keypadStates[ROWS][COLUMNS];
+bool keypadStatesPrev[ROWS][COLUMNS];
+String buttonPressed = "0";
+
 uint16_t buzzerVal = 10;
 unsigned long now = micros();
 unsigned long nextBuzz = 0;
@@ -34,9 +37,7 @@ void setup() {
       keypadStates[r][c] = false;
     }
   }
-
   pinMode(pinBuzzer, OUTPUT);
-
   Serial.println("BOOT!");
 }
 
@@ -46,31 +47,36 @@ Loop through COLUMNS and switch one by one to OUTPUT LOW.
 If a row turns LOW as well we can pinpoint button.
 */
 void loop() {
+  checkMatrix();
+  if (keypadStates != keypadStatesPrev) {
+    Serial.println(buttonPressed);
+  }
+  for (int c = 0; c < COLUMNS; c++) {
+    for (int r = 0; r < ROWS; r++) {
+      keypadStatesPrev[r][c] = keypadStates[r][c];
+    }
+  }
+}
+
+void checkMatrix() {
   for (int c = 0; c < COLUMNS; c++) {
     pinMode(columnPins[c], OUTPUT);
     digitalWrite(columnPins[c], LOW);
     for (int r = 0; r < ROWS; r++) {
       if (digitalRead(rowPins[r]) == LOW) {
         keypadStates[r][c] = true;
-        Serial.println(keypadValues[r][c]);
-        if (keypadValues[r][c] == 1) buzzerVal = 100;
-        if (keypadValues[r][c] == 2) buzzerVal = 300;
-        if (keypadValues[r][c] == 3) buzzerVal = 600;
-        if (keypadValues[r][c] == 4) buzzerVal = 1000;
+        buttonPressed = keypadValues[r][c];
       } else keypadStates[r][c] = false;
     }
     pinMode(columnPins[c], INPUT_PULLUP);
   }
-  digitalWrite(pinBuzzer, HIGH);
-  delayMicroseconds(buzzerVal);
-  digitalWrite(pinBuzzer, LOW);
 }
 
-void buzz(uint16_t buzzerVal) {
-  now = micros();
-  nextBuzz = 
+// void buzz(uint16_t buzzerVal) {
+//   now = micros();
+//   nextBuzz = 
   
-}
+// }
 
 // // MODELL B
 // void setup() {
---
+++ 04-Digitalteknik-och-Elektronik/labb/keypad2/keypad2/keypad2.ino
@@ -0,0 +1,80 @@
+constexpr uint8_t COLUMNS = 2;
+constexpr uint8_t ROWS = 2;
+constexpr uint8_t PIN_COL_1 = 10;
+constexpr uint8_t PIN_COL_2 = 9;
+constexpr uint8_t PIN_ROW_1 = 21;
+constexpr uint8_t PIN_ROW_2 = 20;
+const int columnPins[COLUMNS] = {PIN_COL_1, PIN_COL_2};
+const int rowPins[ROWS] = {PIN_ROW_1,PIN_ROW_2};
+const int keypadValues[ROWS][COLUMNS] = {{1, 2}, {3, 4}};
+bool keypadStates[ROWS][COLUMNS];
+bool keypadStatesPrev[ROWS][COLUMNS];
+String buttonVal = "0";
+unsigned long now = 0;
+unsigned long buttonPressedMillis = 0;
+unsigned long buttonHoldThreashold = 300;
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) delay(10);
+
+  for (auto& c : columnPins) {
+    pinMode(c, INPUT_PULLUP);
+  }
+  for (auto& r : rowPins) {
+    pinMode(r, INPUT_PULLUP);
+  }
+  memset(keypadStates, 0, sizeof(keypadStates));
+  Serial.println("BOOT!");
+}
+
+void loop() {
+  checkMatrix();
+  if (keypadStatesHasChanged()) {
+    Serial.println(buttonVal);
+  }
+  setPrevStates();
+  delay(50);
+}
+
+void checkMatrix() {
+  for (int c = 0; c < COLUMNS; c++) {
+    pinMode(columnPins[c], OUTPUT);
+    digitalWrite(columnPins[c], LOW);
+    for (int r = 0; r < ROWS; r++) {
+      if (digitalRead(rowPins[r]) == LOW) {
+        keypadStates[r][c] = true;
+        buttonVal = keypadValues[r][c];
+      } else keypadStates[r][c] = false;
+    }
+    pinMode(columnPins[c], INPUT_PULLUP);
+  }
+}
+
+bool keypadStatesHasChanged() {
+  now = millis();
+
+  for (int c = 0; c < COLUMNS; c++) {
+    for (int r = 0; r < ROWS; r++) {
+      if (keypadStatesPrev[r][c] == false && keypadStates[r][c] == true) {
+        buttonPressedMillis = now;
+      }
+      if (keypadStatesPrev[r][c] == true && keypadStates[r][c] == false && now - buttonPressedMillis >= buttonHoldThreashold) {
+        Serial.print("Hold: ");
+        return true;
+      } else if (keypadStatesPrev[r][c] == true && keypadStates[r][c] == false) {
+        Serial.print("Pressed: ");
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+void setPrevStates() {
+  for (int c = 0; c < COLUMNS; c++) {
+    for (int r = 0; r < ROWS; r++) {
+      keypadStatesPrev[r][c] = keypadStates[r][c];
+    }
+  }
+}
