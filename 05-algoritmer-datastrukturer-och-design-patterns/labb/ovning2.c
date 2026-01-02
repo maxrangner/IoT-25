30d4ef5ca4a2b1434eab3ddadc1f13c2b5d72975
maxrangner <max@rangner.com>
2026-01-02 16:53:07 +0100

    Update labb.

--- 05-algoritmer-datastrukturer-och-design-patterns/labb/ovning2.c
+++ 05-algoritmer-datastrukturer-och-design-patterns/labb/ovning2.c
@@ -8,22 +8,34 @@ b)	skriv funktionen goodFib(int n) som returnerar fibbonacitalet för n.
 
 	tips: anropet från goodFib() till fibSub() är antagligen något i stil med
 	fibSub(0, 1, n)
+
     0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55...n
 */
 
 #include <stdio.h>
 
-int badFib(int n) {
-    if (n == 0) return 0;
-    if (n == 1) return 1;
-    return badFib(n - 1) + badFib(n - 2);
+// int badFib(int n) {
+//     if (n == 0) return 0;
+//     if (n == 1) return 1;
+//     return badFib(n - 1) + badFib(n - 2);
+// }
+
+int fibSub(int a, int b, int n) {
+    return fibSub(a, b, n) + fibSub(a, b, n - 1);
+}
+
+int goodFib(int n) { // 6 == 8
+    int a = n - 1;
+    int b = n - 2;
+    return fibSub(a, b, n);
 }
 
 int main() {
     int n;
     scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        printf("%d: %d\n", i, badFib(i));
-    }
+    // for (int i = 0; i < n; i++) {
+    //     printf("%d: %d\n", i, badFib(i));
+    // }
+    printf("%d", goodFib(n));
     return 0;
 }
