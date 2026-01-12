e0846b8c5e1b96f52e95e103b1b98ffa2823b7d6
maxrangner <max@rangner.com>
2026-01-12 17:53:46 +0100

    05-labb: Ovning-3 update.

--- 05-algoritmer-datastrukturer-och-design-patterns/labb/ovning-3/list.c
+++ 05-algoritmer-datastrukturer-och-design-patterns/labb/ovning-3/list.c
@@ -24,27 +24,29 @@ int isEmpty(List l) {
 int addFirst(List* l, Data d) {
 	struct node* newNode = createListNode(d);
 	if (newNode == NULL) return 0;
-	if ((*l) == NULL) {
+	if (*l == NULL) {
 		*l = newNode;
 	} else {
 		struct node* tempNode = *l;
-		(*l) = newNode;
+		*l = newNode;
 		(*l)->rest = tempNode;
 	}
 	return 1;
 }
 
 int addLast(List* l, Data d) {
-	int idx = 0;
-	struct node* lastNode = *l;
-	if (lastNode == NULL) return 0;
+	struct node* currentNode = *l;
+	if (isEmpty(*l)) {
+		*l = createListNode(d);
+		return 1;
+	}
 	while(1) {
-		if (lastNode->rest == NULL) {
-			lastNode->rest = createListNode(d);
-			if (lastNode->rest == NULL) return 0;
+		if (currentNode->rest == NULL) {
+			currentNode->rest = createListNode(d);
+			if (currentNode->rest == NULL) return 0;
 			break;
 		} else {
-			lastNode = lastNode->rest;
+			currentNode = currentNode->rest;
 		}
 	}
 	return 1;
@@ -52,44 +54,104 @@ int addLast(List* l, Data d) {
 
 void removeFirst(List* l) {
 	assert(!isEmpty(*l));
+	struct node* toDestroy = *l;
 	*l = (*l)->rest;
-
+	free(toDestroy);
 }
 
 void removeLast(List* l) {
-	// Tänk på att du måste hantera fallet där listan bara har ett element.
-	//precondition: listan är inte tom (testa med assert)
+	assert(!isEmpty(*l));
+	if (isEmpty(*l)) return;
+	
+	struct node* temp = *l;
+	if (temp->rest == NULL) {
+		*l = NULL;
+		return;
+	}
+	while(temp->rest->rest != NULL) {
+			temp = temp->rest;
+	}
+	struct node* toDestroy = temp->rest;
+	temp->rest = NULL;
+	free(toDestroy);
 }
 
 int removeElement(List* l, Data d) {
-	// Se till att ta bort första noden korrekt. Behöver ha koll på föregående nod när du itererar.
-	return 0; //endast här för kompilering
+	assert(!isEmpty(*l));
+	if (isEmpty(*l)) return 0;
+
+	struct node* temp = *l;
+	struct node* toDestroy = temp;
+	if (temp->d == d) {
+		*l = (*l)->rest;
+		free(toDestroy);
+		return 1;
+	}
+	while(temp->rest != NULL && temp->rest->d != d) {
+		temp = temp->rest;
+	}
+	if (temp->rest == NULL) return 0;
+	toDestroy = temp->rest;
+	temp->rest = temp->rest->rest;
+	free(toDestroy);
+	return 1;
 }
 
 int isInList(List l, Data d) {
-	return 0; //endast här för kompilering
+	struct node* currentElement = l;
+	while(currentElement != NULL) {
+		if (currentElement->d == d) return 1;
+		currentElement = currentElement->rest;
+	}
+	return 0;
 }
 
 int lengthOfList(List l) {
-	return 0; //endast här för kompilering
+	struct node* currentElement = l;
+	int idx = 0;
+	if (currentElement == NULL) return idx;
+	idx++;
+	while(currentElement->rest != NULL) {
+		currentElement = currentElement->rest;
+		idx++;
+	}
+	return idx;
 }
 
 void clearList(List* l) {
-	// Enklast och säkrast att köra removeFirst i en loop tills listan är tom.
-	//postcondition: listan är tom (testa med assert)
+	while((*l) != NULL) {
+		removeFirst(l);
+	}
+	assert(isEmpty(*l));
 }
 
 void printList(List l, FILE* stream) {
-	// Tom lista ska skriva ut: []
-	// Icke-tom lista ska skriva ut: [data1, data2, data3]
+	if (l == NULL) {
+
+		return;
+	}
+	List currentNode = l;
+	printf("[");
+	while(1) {
+		printf("%d", (currentNode)->d);
+		if ((currentNode)->rest == NULL) break;
+		printf(", ");
+		(currentNode) = (currentNode)->rest;
+	}
+	printf("]");
 }
 
 Data getFirstElement(List l) {
-	//precondition: listan är inte tom (testa med assert)
-	return 0; //endast här för kompilering
+	assert(!isEmpty(l));
+	return l->d;
 }
 
 Data getLastElement(List l) {
-	//precondition: listan är inte tom (testa med assert)
-	return 0; //endast här för kompilering
+	assert(!isEmpty(l));
+		
+	struct node* temp = l;
+	while(temp->rest != NULL) {
+			temp = temp->rest;
+	}
+	return temp->d;
 }
--- 05-algoritmer-datastrukturer-och-design-patterns/labb/ovning-3/list.h
+++ 05-algoritmer-datastrukturer-och-design-patterns/labb/ovning-3/list.h
@@ -15,17 +15,8 @@ typedef struct node* List;
 List createEmptyList(void);
 int isEmpty(List l);
 int addFirst(List* l, Data d);
-
-//lägger till 'd' sist i listan
-//returnerar 1 ifall lyckades, annars 0
-// O(n) med nuvarande design
 int addLast(List* l, Data d);
-
-//tar bort första noden från listan
 void removeFirst(List* l);
-
-//tar bort sista noden från listan
-// O(n), måste gå igenom hela listan för att hitta näst sista noden med nuvarande design
 void removeLast(List* l);
 
 //tar bort första förekomsten av 'd' från listan
