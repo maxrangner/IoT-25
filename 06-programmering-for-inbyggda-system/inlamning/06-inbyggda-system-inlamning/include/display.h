35ec90c4299a3225228567cbdd1edd11c7ed4a41
maxrangner <max@rangner.com>
2026-03-12 21:18:48 +0100

    Clean up project

--- 06-programmering-for-inbyggda-system/inlamning/06-inbyggda-system-inlamning/include/display.h
+++ 06-programmering-for-inbyggda-system/inlamning/06-inbyggda-system-inlamning/include/display.h
@@ -6,15 +6,5 @@
 
 void display_init(void);
 void display_billboard(Billboard* next_billboard, uint32_t now);
-void display_rows(Billboard* next_billboard, char* top, char* bottom);
-void add_white_space_padding(
-    Billboard* next_billboard,
-    char* top,
-    char* bottom,
-    char* top_padded,
-    char* bottom_padded
-);
-void create_scroll_frame(char* buffer_display, char* buffer_padded, uint8_t scroll_pos);
-void display_scroll_frame(char* top_display, char* top_padded, char* bottom_display, char* bottom_padded, uint8_t* scroll_pos);
 
 #endif
--- 06-programmering-for-inbyggda-system/inlamning/06-inbyggda-system-inlamning/src/main.c
+++ 06-programmering-for-inbyggda-system/inlamning/06-inbyggda-system-inlamning/src/main.c
@@ -1,20 +1,15 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <util/delay.h>
-#include <avr/io.h>
 #include "client_manager.h"
 #include "display.h"
 #include "millis.h"
-#include "lcd.h"
 #include "utils.h"
 #include "uart.h"
 
 int main(void) {
     ClientManager mgr;
     uint8_t random_seed;
-    uint32_t update_interval = 10000;
     uint32_t now = 0;
-    uint32_t prev_update = 0 - update_interval;
+    uint32_t prev_update = now - MAIN_UPDATE_INTERVAL;
     Billboard* next_billboard;
     static FILE uart_stdout = FDEV_SETUP_STREAM(uart_putchar, NULL, _FDEV_SETUP_WRITE);
     
@@ -25,14 +20,9 @@ int main(void) {
     client_manager_init(&mgr);
     display_init();
 
-    Billboard temp_bill = {
-        .billboard_effect = blink,
-        .billboard_text = "Mysterier? Ring Langben"
-    };
-
     while(1) {
         now = millis();
-        if ((now - prev_update) >= update_interval) {
+        if ((now - prev_update) >= MAIN_UPDATE_INTERVAL) {
             next_billboard = get_next_billboard(&mgr, now);
             prev_update = now;
         }
